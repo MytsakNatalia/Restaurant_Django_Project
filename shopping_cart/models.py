0c26202a6d7f8a1294efa0dca88497728b1193dc
Natalia Mytsak <mytsaknat@gmail.com>
2024-04-19 12:08:25 +0300

    Order model created

--- shopping_cart/models.py
+++ shopping_cart/models.py
@@ -11,3 +11,24 @@ class ShoppingCartItem(models.Model):
     total_price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
 
 
+class Order(models.Model):
+    WAITING = 'Waiting for processing'
+    IN_PROCESS = 'In process'
+    COMPLETED = 'Completed'
+    CANCELED = 'Canceled'
+    DECLINED = 'Declined'
+
+    STATUS_CHOICES = [
+        (WAITING, 'Waiting for processing'),
+        (IN_PROCESS, 'In process'),
+        (COMPLETED, 'Completed'),
+        (CANCELED, 'Canceled'),
+        (DECLINED, 'Declined'),
+    ]
+
+    user = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=256)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    shopping_cart = models.ForeignKey(to=ShoppingCart, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=WAITING)
+
