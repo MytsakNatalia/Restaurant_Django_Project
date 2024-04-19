67c0a44db9266c9657fc02ec53b78258a409f17f
Natalia Mytsak <mytsaknat@gmail.com>
2024-04-19 12:10:29 +0300

    Models registered for admin

--- shopping_cart/admin.py
+++ shopping_cart/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from shopping_cart.models import ShoppingCart, ShoppingCartItem, Order
 # Register your models here.
+admin.site.register(ShoppingCart)
+admin.site.register(ShoppingCartItem)
+admin.site.register(Order)
