73411ce151d9a2c2b46706c6278737f832709528
alevchenko12 <nastiya.levchenko@gmail.com>
2024-04-28 16:22:34 +0200

    Added web page for Menu (only for view)

--- restaurant/menu/views.py
+++ restaurant/menu/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from menu.models import Category, Meal, Product, MealProduct
 
-# Create your views here.
+def index(request):
+    context = {
+        'title': 'Menu',
+        'categories': Category.objects.all(), 
+        'meals': Meal.objects.all(),
+    }
+    return render(request, 'menu/index.html', context)
+
+def meals(request):
+    context = {
+        'title': 'List of meals',
+        'categories': Category.objects.all(), 
+        'meals': Meal.objects.all(),
+    }
+    return render (request, 'menu/meals.html', context)
+
--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +124,10 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR/'media'
+#MEDIA_ROOT = os.path.join(BASE_DIR)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
