f683ed174210f74a6ea45cdb6299743e21790c9d
Natalia Mytsak <mytsaknat@gmail.com>
2024-05-02 18:27:09 +0300

    Add to cart method created

--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -20,6 +20,7 @@ from menu.views import *
 from django.conf.urls.static import static
 from django.conf import settings
 from users.views import login
+from  shopping_cart.views import view_cart, add_to_cart
 
 urlpatterns = [
     path('', login, name='login'),
@@ -27,6 +28,8 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='users')),
     path('menu/', index, name='index'),    
     path('menu/details/<int:meal_id>/', meal_detail, name='meal_detail'),
+    path('view_cart', view_cart, name='view_cart'),
+    path('add_to_cart/<int:meal_id>/', add_to_cart, name='add_to_cart'),
 ]
 
 if settings.DEBUG:
--- restaurant/shopping_cart/views.py
+++ restaurant/shopping_cart/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from  .models import ShoppingCart, ShoppingCartItem
+from menu.models import Meal
 # Create your views here.
+def view_cart(request):
+    user_cart = ShoppingCart.objects.get(user=request.user, is_active = True)
+    cart_items = ShoppingCartItem.objects.filter(shopping_cart = user_cart)
+
+    context = {
+        'shopping_cart': user_cart,
+        'cart_items': cart_items
+    }
+    return render(request, 'shopping_cart/view_cart.html', context)
+
+def add_to_cart(request, meal_id):
+    meal = Meal.objects.get(id = meal_id)
+    user_cart = ShoppingCart.objects.get(user=request.user, is_active=True)
+    meal_cart_items = ShoppingCartItem.objects.filter(shopping_cart= user_cart, meal= meal)
+    if not meal_cart_items.exists():
+        ShoppingCartItem.objects.create(shopping_cart=user_cart, meal=meal, quantity=1, total_price=meal.price)
+    else:
+        cart_item = meal_cart_items.first()
+        cart_item.quantity +=1
+        cart_item.total_price = cart_item.quantity*cart_item.meal.price
+        cart_item.save()
+    return redirect(request.META.get('HTTP_REFERER', 'menu/'))
