56a3e30662c800d265b97f17aa33111153ef56c9
alevchenko12 <nastiya.levchenko@gmail.com>
2024-05-03 14:59:05 +0200

    Added fully filtering

--- restaurant/menu/views.py
+++ restaurant/menu/views.py
@@ -1,47 +1,32 @@
 from django.shortcuts import render,  get_object_or_404
 from menu.models import Category, Meal, Product, MealProduct
 
-'''def index(request):
-    categories = Category.objects.all()  
-    meals = Meal.objects.all()  
-
-    context = {
-        'title': 'Menu',
-        'categories': categories, 
-        'meals': meals,
-    }
-    return render(request, 'menu/index.html', context)'''
-    
-'''def index(request, category_id=None):
-    if category_id:
-        category = Category.objects.get(id=category_id)
-        meals = Meal.objects.filter(category=category)
-    else: 
-        categories = Category.objects.all()  
-        meals = Meal.objects.all()  
-
-    context = {
-        'title': 'Menu',
-        'categories': categories, 
-        'meals': meals,
-    }
-    return render(request, 'menu/index.html', context)'''
-    
 def index(request, category_id=None):
     categories = Category.objects.all()
     meals = Meal.objects.all()
-
+    
     if category_id:
         category = Category.objects.get(pk=category_id)
         meals = meals.filter(category=category)
+    
+    vegan = request.GET.get('vegan')
+    if vegan:
+        meals = meals.filter(is_vegan=True)
+    
+    spicy = request.GET.get('spicy')
+    if spicy:
+        meals = meals.filter(is_spicy=True)
 
     context = {
         'title': 'Menu',
         'categories': categories,
         'meals': meals,
-        'selected_category_id': category_id  
+        'selected_category_id': category_id,
+        'vegan': vegan,
+        'spicy': spicy 
     }
     return render(request, 'menu/index.html', context)
+
 
 def meal_detail(request, meal_id):
     meal = get_object_or_404(Meal, pk=meal_id)
